298074fd1075eea338876e50bf335a6eecb1101e
Victoria-Waltz <vwaltz0104@gmail.com>
2026-09-08 18:02:01 -0400

    Add minimum balance requirement

--- BankAccount.java
+++ BankAccount.java
@@ -13,10 +13,10 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise.
+        if (balance - amount >= 50) {
+            balance -= amount;
+            return true;
+        }
         return false;
     }
 
