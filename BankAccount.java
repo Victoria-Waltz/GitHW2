6a14be5d345ff75ff027c94b84adfdc4cc9ff1f7
Victoria-Waltz <vwaltz0104@gmail.com>
2026-09-08 18:10:17 -0400

    Add transaction fee to withdrawals

--- BankAccount.java
+++ BankAccount.java
@@ -13,10 +13,11 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise.
+        double total = amount + 2;
+        if (balance >= total) {
+            balance -= total;
+            return true;
+        }
         return false;
     }
 
